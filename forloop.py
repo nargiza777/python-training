7caafa456833874586896fb97dbe22b884eba84e
NargizaOsmon <nargizaosmon@gmail.com>
2023-01-03 19:28:51 -0600

    initial commit

--- forloop.py
+++ forloop.py
@@ -1,13 +1,13 @@
 #035 ask the user to enter their name and then display their name three times
-# name = input("Enter your name?")
-# for i in range(3):
-#     print(name)
+name = input("Enter your name?")
+for i in range(3):
+    print(name)
 
 #036 Alter 035 so that it will ask the user to enter their name and a number and then display their name that number of times
-# name = input("Enter your name?")
-# num = int(input("Enter a number?"))
-# for i in range(0,num):
-#     print(name)
+name = input("Enter your name?")
+num = int(input("Enter a number?"))
+for i in range(0,num):
+    print(name)
 #037 Ask the user to enter their name and display each letter in their name on a separate line.
 name = input("Enter your name?")
 for i in name:
